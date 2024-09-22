7173632d090a114a5a92fbb8a6851456c90c2981
Francisco Galea <galeafrancisco2001@gmail.com>
2024-09-22 19:48:00 -0300

    Added repos

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using TestApi.Data;
+using TestApi.Models.Entities;
+using TestApi.Repositories.IRepository;
+
+namespace TestApi.Repositories
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext context;
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+            this.context = context;
+        }
+    }
+}
