d1a16a945a9753be3e18b7317883b76c6ed6f395
Francisco Galea <galeafrancisco2001@gmail.com>
2024-09-22 20:32:28 -0300

    Added ProductRepository

--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using TestApi.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using TestApi.Data;
 using TestApi.Models.Entities;
 using TestApi.Repositories.IRepository;
 
@@ -11,5 +12,37 @@ namespace TestApi.Repositories
         {
             this.context = context;
         }
+
+        public override async Task Insert(Product product)
+        {
+            await context.Products.AddAsync(product);
+            await context.SaveChangesAsync();
+        }
+
+        //Revisar metodo
+        public override async Task Update(Product product)
+        {
+            context.Products.Update(product);
+            await context.SaveChangesAsync();
+        }
+
+        public override async Task<Product> GetById(int id)
+        {
+            return await context
+                         .Products
+                         .Where(p => p.IsProductActive == true && p.ProductId == id)
+                         .Include(p => p.OCategory)
+                         .FirstAsync();
+        }
+
+        public override async Task<List<Product>> GetAll()
+        {
+            return await context
+                         .Products
+                         .Where(p => p.IsProductActive == true)
+                         .Include(p => p.OCategory)
+                         .ToListAsync();
+        }
+
     }
 }
