dd7d646e220a338728b982e95b47e5c487f1dec3
Francisco Galea <galeafrancisco2001@gmail.com>
2024-09-22 20:32:41 -0300

    Added ProductService

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,6 +1,61 @@
-﻿namespace TestApi.Services
+﻿using AutoMapper;
+using TestApi.Models.DTOs.Request;
+using TestApi.Models.DTOs.Response;
+using TestApi.Models.Entities;
+using TestApi.Repositories.IRepository;
+
+namespace TestApi.Services
 {
     public class ProductService
     {
+        private readonly IMapper mapper;
+        private readonly IProductRepository productRepository;
+
+        public ProductService(IMapper mapper, IProductRepository productRepository)
+        {
+            this.mapper = mapper;
+            this.productRepository = productRepository;
+        }
+
+        public async Task CreateProduct(ProductRequest productRequest)
+        {
+            var product = mapper.Map<Product>(productRequest);
+            product.IsProductActive = true;
+            await productRepository.Insert(product);
+        }
+
+        public async Task UpdateProduct(int id, ProductRequest productRequest)
+        {
+            var existingProduct = await productRepository.GetById(id);
+            if (existingProduct == null)
+            {
+                throw new KeyNotFoundException("No se ha encontrado el producto");
+            }
+            else
+            {
+                mapper.Map(existingProduct, productRequest);
+                await productRepository.Update(existingProduct);
+            }
+        }
+
+        public async Task<ProductResponse> GetProductById(int id)
+        {
+            var existingProduct = await productRepository.GetById(id);
+            if (existingProduct == null)
+            {
+                throw new KeyNotFoundException("No se ha encontrado el producto");
+            }
+            else
+            {
+                return mapper.Map<ProductResponse>(existingProduct);
+            }
+        }
+        public async Task<List<ProductResponse>> GetAllProducts()
+        {
+            var products = await productRepository.GetAll();
+            return mapper.Map<List<ProductResponse>>(products);
+        }
+
+
     }
 }
