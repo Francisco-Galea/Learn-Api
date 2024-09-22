100fba75b6327c12fcaadeaa21cc0cd29ce66e53
Francisco Galea <galeafrancisco2001@gmail.com>
2024-09-22 20:31:33 -0300

    Added product controller

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TestApi.Models.DTOs.Request;
+using TestApi.Models.DTOs.Response;
+using TestApi.Services;
 
 namespace TestApi.Controllers
 {
@@ -7,5 +10,37 @@ namespace TestApi.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly ProductService productService;
+
+        public ProductController(ProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpPost]
+        public async Task CreateProduct([FromBody] ProductRequest productRequest)
+        {
+            await productService.CreateProduct(productRequest);
+        }
+
+        [HttpPut]
+        public async Task UpdateProduct(int id, [FromBody] ProductRequest productRequest)
+        {
+            await productService.UpdateProduct(id, productRequest);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ProductResponse> GetProductById(int id)
+        {
+            return await productService.GetProductById(id);
+        }
+
+        [HttpGet]
+        public async Task<List<ProductResponse>> GetAllProducts()
+        {
+            return await productService.GetAllProducts();
+        }
+
+
     }
 }
